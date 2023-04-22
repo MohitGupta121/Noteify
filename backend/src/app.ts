92b6b955327cf6abcb84588976698ba5c88b554d
MohitGupta121 <guptamm.22@gmail.com>
2023-04-22 21:51:24 +0530

    added get, post endpoints, arrange code, morgan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,15 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import NoteModel from "./models/notes";
+import notesRoutes from "./routes/notes";
+import morgan from "morgan";
 
 const app = express();
 
-app.get("/", async (req, res, next) => {
-  try {
-    // throw Error("This is error by me");
-    const notes = await NoteModel.find().exec();
-    res.status(200).json(notes);
-  } catch (error) {
-    next(error);
-  }
-});
+app.use(morgan("dev"));
 
-app.use((req, res, next) => {
-  next(Error("Endpoint not found"));
-});
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
