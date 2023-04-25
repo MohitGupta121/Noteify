011ec7dcaebdd144da86a9579449a77392a2a21b
MohitGupta121 <guptamm.22@gmail.com>
2023-04-25 12:25:39 +0530

    Auth UI created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,9 @@ import styleUtils from "./styles/utils.module.css"
 import * as NotesApi from "./network/notes_api"
 import AddEditNoteDialog from './components/AddEditNoteDialog';
 import { FaPlus } from "react-icons/fa";
+import SignUpModal from './components/SignUpModal';
+import LoginModal from './components/LoginModal';
+import NavBar from './components/NavBar';
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
@@ -61,43 +64,63 @@ function App() {
     </Row>
 
   return (
-    <Container className={styles.notesPage}>
-      <Button
-        className={`mb-4 ${styleUtils.blockCenter} ${styleUtils.flexCenter}`}
-        onClick={() => setShowAddNoteDialog(true)}>
-        <FaPlus />
-        Add new note
-      </Button>
-      {notesLoading && <Spinner animation='border' variant='primary' />}
-      {showNotesLoadingError && <p>Something went wrong. Please refresh the page</p>}
-      {!notesLoading && !showNotesLoadingError &&
-        <>
-          {notes.length > 0
-            ? notesGrid
-            : <p>You don't have any notes yet</p>
+    <div>
+      <NavBar
+        loggedInUser={null}
+        onLoginClicked={() => { }}
+        onSignUpClicked={() => { }}
+        onLogoutSuccessful={() => { }}
+      />
+      <Container className={styles.notesPage}>
+        <Button
+          className={`mb-4 ${styleUtils.blockCenter} ${styleUtils.flexCenter}`}
+          onClick={() => setShowAddNoteDialog(true)}>
+          <FaPlus />
+          Add new note
+        </Button>
+        {notesLoading && <Spinner animation='border' variant='primary' />}
+        {showNotesLoadingError && <p>Something went wrong. Please refresh the page</p>}
+        {!notesLoading && !showNotesLoadingError &&
+          <>
+            {notes.length > 0
+              ? notesGrid
+              : <p>You don't have any notes yet</p>
 
-          }
-        </>
-      }
-      {showAddNoteDialog &&
-        <AddEditNoteDialog
-          onDismiss={() => setShowAddNoteDialog(false)}
-          onNoteSaved={(newNote) => {
-            setNotes([...notes, newNote])
-            setShowAddNoteDialog(false);
-          }} />
-      }
-      {noteToEdit &&
-        <AddEditNoteDialog
-          noteToEdit={noteToEdit}
-          onDismiss={() => setNoteToEdit(null)}
-          onNoteSaved={(updatedNote) => {
-            setNotes(notes.map(existingNote => existingNote._id === updatedNote._id ? updatedNote : existingNote));
-            setNoteToEdit(null);
-          }}
-        />
-      }
-    </Container>
+            }
+          </>
+        }
+        {showAddNoteDialog &&
+          <AddEditNoteDialog
+            onDismiss={() => setShowAddNoteDialog(false)}
+            onNoteSaved={(newNote) => {
+              setNotes([...notes, newNote])
+              setShowAddNoteDialog(false);
+            }} />
+        }
+        {noteToEdit &&
+          <AddEditNoteDialog
+            noteToEdit={noteToEdit}
+            onDismiss={() => setNoteToEdit(null)}
+            onNoteSaved={(updatedNote) => {
+              setNotes(notes.map(existingNote => existingNote._id === updatedNote._id ? updatedNote : existingNote));
+              setNoteToEdit(null);
+            }}
+          />
+        }
+        {false &&
+          <SignUpModal
+            onDismiss={() => { }}
+            onSignUpSuccessful={() => { }}
+          />
+        }
+        {false &&
+          <LoginModal
+            onDismiss={() => { }}
+            onLoginSuccessful={() => { }}
+          />
+        }
+      </Container>
+    </div>
   );
 }
 
