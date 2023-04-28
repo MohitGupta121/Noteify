f98373808955370e321e17777d6852fc06961607
MohitGupta121 <guptamm.22@gmail.com>
2023-04-28 18:18:44 +0530

    title updated

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -16,7 +16,7 @@ const NavBar = ({ loggedInUser, onSignUpClicked, onLoginClicked, onLogoutSuccess
         <Navbar bg="primary" variant="dark" expand="sm" sticky="top">
             <Container>
                 <Navbar.Brand as={Link} to="/">
-                    Cool Notes App
+                    Noteify - All your notes
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="main-navbar" />
                 <Navbar.Collapse id="main-navbar">
